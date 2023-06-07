175b7b4cc6cbdfa701d0990ca0649311e3f4673f
Max_White_魚 <103893102+RubyCat1337@users.noreply.github.com>
2023-06-07 13:14:22 +0300

    Add files via upload

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
+from .models import Product, Category, Comment
 from django.shortcuts import render
 
 # Create your views here.
 def show_catalog(request):
-    respose = render(request, "catalogapp/catalog.html")
+    context = {"list_products": Product.objects.all(), 'additional_category': Category.objects.all()}
+    respose = render(request, "catalogapp/catalog.html", context)
     return respose
