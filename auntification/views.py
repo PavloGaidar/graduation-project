7ea5b012f928da3665facbce27951cf75e96bb6b
pasha <justuser166@gmail.com>
2023-06-07 20:03:22 +0300

    1

--- auntification/views.py
+++ auntification/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import User
 
+
 # Create your views here.
 def show_registration(request):
     response  = render(request, "auntificationapp/reg.html")
@@ -14,33 +15,21 @@ def show_registration(request):
         return response
     return response 
 
-
-
-
 def show_login(request):
-    response  = render(request, "auntificationapp/login.html")
+    response = render(request, "auntificationapp/login.html")
     if request.method == "GET" and request.COOKIES.get('LogIn') is not None:
         return redirect('../')
-    if request.method == "POST":
-        name = request.POST.get('name')
-        password = request.POST.get('password')
-        user = User(request, name = name, password = password)
-        
-    return response
-    
-def show_login(request):
-    context = {}
-    response  = render(request, "auntificationapp/login.html")
     if request.method == "POST":
         name = request.POST.get("name")
         password = request.POST.get("password")
 
-        user = User(request, name = name, password = password)
+        user = User.objects.filter(name=name, password=password)
+        print(user)
         if user != None:
+            print('пройшло')
             response.set_cookie('LogIn', True)
-            context["error_text"] = ""
             return response
-            
         else:
-            context["error_text"] = "Oh no, There was mistake nickname or password are incorect"
+            print('не пройшло')
+            response = render(request, "auntificationapp/login.html", context={'error' : 'true'})
     return response
