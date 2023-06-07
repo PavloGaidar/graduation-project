8ecfb07fb1e1c94fad040d0bd96c7f19d993f463
pasha <justuser166@gmail.com>
2023-06-07 21:03:49 +0300

    done registration

--- auntification/views.py
+++ auntification/views.py
@@ -1,18 +1,51 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect 
 from .models import User
+from django.db.utils import IntegrityError
 
 
 # Create your views here.
 def show_registration(request):
+    context={}
     response  = render(request, "auntificationapp/reg.html")
     if request.method == "GET" and request.COOKIES.get('LogIn') is not None:
         return redirect('../')
     if request.method == "POST":
-        # записуємо данні з форми в змінні
-        UserNew = User(name=request.POST.get('name'),password=request.POST.get('password'), phone=request.POST.get('phone'),email=request.POST.get('email'))
-        UserNew.save()
-        response.set_cookie('LogIn', True)
-        return response
+        name=request.POST.get('name')
+        password = request.POST.get('password')
+        password_confirm = request.POST.get('password_confirm')
+        phone=request.POST.get('phone')
+        email=request.POST.get('email')
+        users = User.objects.all()
+        for user in users:
+            if name != user.name:
+                if email != user.email:
+                    if phone != user.phone:
+                        UserNew = User(name=request.POST.get('name'),password=request.POST.get('password'),phone=request.POST.get('phone'),email=request.POST.get('email'))
+                        UserNew.save()
+                        response.set_cookie('LogIn', True)
+                        print('succes')
+                        response  = render(request, "auntificationapp/reg.html")
+                        return response
+                    else:
+                        context['error_text']= 'Phone number is already taken'
+                        print("phone")
+                        response  = render(request, "auntificationapp/reg.html", context)
+                        return response
+                else:
+                    context['error_text']= 'Email is already taken'
+                    print("email")
+                    response  = render(request, "auntificationapp/reg.html", context)
+                    return response
+            else:
+                context['error_text']= 'Nickname is already taken'
+                print("name")
+                response  = render(request, "auntificationapp/reg.html", context)
+                return response
+        else:
+            context['error_text']= 'Паролі не співпадають!'
+            response  = render(request, "auntificationapp/reg.html", context)
+            return response
+
     return response 
 
 def show_login(request):
