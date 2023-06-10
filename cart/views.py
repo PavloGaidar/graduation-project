4ba9dd735ee7258a1aef43815afe4fd0dd0f8507
Max_White_魚 <103893102+RubyCat1337@users.noreply.github.com>
2023-06-10 18:26:16 +0300

    cart

--- cart/views.py
+++ cart/views.py
@@ -3,36 +3,32 @@ from django.http import HttpResponse
 from catalog.models import Product
 
 # Create your views here.
+
+
+
 def show_cart(request):
-    if request.COOKIES.get('product') is not None: #
-        
-        products = request.COOKIES['product']
-        products = products.split(' ')
-
-        list_products = list()
-        for product in products:
-            list_products.append(Product.objects.get(pk=product))
-        response = render(request,"cartapp/cart.html",context={"products": list_products})
-    else:
-        response = render(request,"cartapp/cart.html",context={"products":list()})
-
-        
-    if request.method == "POST":
-        print(request.POST.get('index'))
-        # products = request.COOKIES['product'].split(' ')
-        # if request.POST.get('name') == 'delete':
-        #     if len(request.COOKIES['product'].split(' ')) > 1:
-        #         product = request.POST.get('item') + "_" + request.POST.get('amount')
-        #         products.remove(product)
-        #         products = " ".join(products)
-        #         print(products)
-        #         response.set_cookie('product', products)
-        #         return response
-        #     else:
-        #         response.delete_cookie('product')
-        #         return response
-
-
-    return response
-
     
+
+
+
+
+        products_pk = request.COOKIES['product'].split(' ')
+        list_products = Product.objects.filter(pk__in=products_pk)
+        response = render(request, 'cartapp/cart.html', {'products': list_products})
+
+        if request.method == 'POST':
+            pk_deleted = request.POST.get('product_pk')
+
+            if pk_deleted:
+                products_pk.remove(pk_deleted)
+                new_product = ' '.join(products_pk)
+
+                if new_product:
+                    list_products = Product.objects.filter(pk__in=products_pk)
+                    response.set_cookie('product_pk', new_product)
+                    
+                else:
+                    response = render(request, 'cartapp/cart.html', {'products': []})
+                    response.delete_cookie('product_pk')
+
+        return response
