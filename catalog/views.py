c0bc1254c73ae7fb06747ada25f70f40233ea62f
Max_White_魚 <103893102+RubyCat1337@users.noreply.github.com>
2023-06-14 09:06:26 +0300

    coment

--- catalog/views.py
+++ catalog/views.py
@@ -7,25 +7,50 @@ def show_catalog(request):
     context = {"list_products": Product.objects.all(), 'additional_category': Category.objects.all()}
     respose = render(request, "catalogapp/catalog.html",  context)
     return respose
+
 def show_product(request, product_pk):
-    product = Product.objects.get(pk=product_pk)
-    response = render(request, 'catalogapp/product.html',context={'product':product})
+    product = get_object_or_404(Product, pk=product_pk)
+    context = {
+        'product': product,
+    }
+
+    response = render(request, 'catalogapp/product.html', context)
+
     if request.method == 'POST':
         name = request.POST.get('name-massages')
-        massages = request.POST.get('messages')
-        Comment.objects.create(name = name, messages = massages,product=product_pk)
-       
+        messages = request.POST.get('messages')
+        Comment.objects.create(name=name, messages=messages, product=product)
 
-
-
-
-
-        if request.COOKIES.get('product') == None:
-            product = product_pk
-            response.set_cookie('product', product)
-            return response
+        if request.COOKIES.get('product') is None:
+            product_cookie = str(product_pk)
         else:
-            product = request.COOKIES['product'] + ' ' + str(product_pk)
-            response.set_cookie('product', product)
-            return response
+            product_cookie = request.COOKIES['product'] + ' ' + str(product_pk)
+        
+        response.set_cookie('product', product_cookie)
+        
+        return response
+
     return response
+# def show_product(request, product_pk):
+#     product = Product.objects.get(pk=product_pk)
+#     context = {
+#         'product':product,
+
+#     }
+
+#     response = render(request, 'catalogapp/product.html',context)
+
+#     if request.method == 'POST':
+#         name = request.POST.get('name-massages')
+#         massages = request.POST.get('messages')
+#         Comment.objects.create(name = name, messages = massages,product=Comment.objects.filter(pk=product_pk) )
+       
+#         if request.COOKIES.get('product') == None:
+#             product = product_pk
+#             response.set_cookie('product', product)
+#             return response
+#         else:
+#             product = request.COOKIES['product'] + ' ' + str(product_pk)
+#             response.set_cookie('product', product)
+#             return response
+#     return response
