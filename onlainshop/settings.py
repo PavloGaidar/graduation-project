d1ba4b4a5487f5bbf0dbf78121644481b07ee4b9
pasha <justuser166@gmail.com>
2023-06-16 21:11:56 +0300

    sfw

--- onlainshop/settings.py
+++ onlainshop/settings.py
@@ -143,6 +143,10 @@ MEDIA_URL = 'media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
+TELEGRAM_BOT_TOKEN = "AAG_R57pVKswrT6Fpm1pOZg3g_xvovRYif4"
+
+# TELEGRAM_BOT_CHAT_ID = 6120371861
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
