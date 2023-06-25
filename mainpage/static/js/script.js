802b50e1cf3bb18274a80abaea7986777dbd019e
pasha <justuser166@gmail.com>
2023-06-25 08:42:52 +0300

    dsa

--- mainpage/static/js/script.js
+++
@@ -1,8 +0,0 @@
-const countProductInCart = document.querySelector("#countProductInCart");
-
-if (countProductInCart.textContent == "None"){
-    countProductInCart.style.display = "none";
-}
-else{
-    countProductInCart.style.display = "flex";
-}
